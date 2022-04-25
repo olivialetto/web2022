8dbf7c444b524f5144a8a45e19871a11cfaf85c5
Olivia Letto <olle224@uky.edu>
2022-04-24 22:19:18 -0400

    adding javascript

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+
+
+if (new Date().getHours() > 18) {
+//alert(new Date().getHours());
+const html = document.querySelector('html');
+html.style.backgroundColor = 'black';
+html.style.color = 'white';
+}
+if (new Date().getHours() > 18) {
+const wrapperBody = document.getElementsByClassName('wrapper');
+wrapperBody[0].style.backgroundColor = 'black';
+wrapperBody[0].style.color = 'white';
+}
+if (new Date().getHours() > 18) {
+const secondaryBody = document.getElementById('secondary');
+secondaryBody.style.backgroundColor = 'black';
+secondaryBody.style.color = 'white';
+}
